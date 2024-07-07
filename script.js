088c016a2ae7d9c35ea03cae8f5ebf6feb438d55
git config --global user.name lambrugeorgegit config --global user.name <lambru_george@yahoo.com>
2024-07-07 16:09:47 +0300

    create a functional add Item

--- script.js
+++ script.js
@@ -91,7 +91,33 @@ onHoldListArray.forEach((onHoldItem, index) => {
 updatedOnLoad = true;
 updateSavedColumns();
 
+//Add to Column List, Reset TextBox
+const addToColumn=(column)=> {
+  const itemText = addItems[column].textContent;
+  const selectedArray = listArrays[column];
+  selectedArray.push(itemText);
+  addItems[column].textContent = '';
+  updateDOM();
+}
 
+
+
+//Show add item input box
+
+const showInputBox=(column)=>{
+  addBtns[column].style.visibility = 'hidden';
+  saveItemBtns[column].style.display = 'flex';
+  addItemContainers[column].style.display = 'flex';
+}
+
+//Hide Item Input  Box
+const hideInputBox=(column)=> {
+  addBtns[column].style.visibility = 'visible';
+  saveItemBtns[column].style.display = 'none';
+  addItemContainers[column].style.display = 'none';
+  addToColumn(column);
+
+}
 
 // Allows arrays to reflect Drag and Drop Items
 const rebuildArrays=() => {
